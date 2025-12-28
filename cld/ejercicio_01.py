63bfa1fbb2fc4a294906ea90b5632a97d089450b
Gaston Buedo <gbuedo@gmail.com>
2025-12-28 01:07:59 -0300

    nueva actualizacion de ejercicios ultimos incluyendo los de Claude

---
+++ cld/ejercicio_01.py
@@ -0,0 +1,9 @@
+nombre = 'gaston'
+edad = 47
+altura = 1.89 # altura en metros
+experiencia_python = True
+
+print("nombre:", nombre, "tipo:", type(nombre))
+print("edad:", edad, "tipo:", type(edad))
+print("altura:", altura, "tipo:", type(altura))
+print("experiencia en Python:", experiencia_python, "tipo:", type(experiencia_python))
---
+++ cld/ejercicio_02.py
@@ -0,0 +1,21 @@
+# Ejercicio 2: Operaciones basicas de calculos y concatenacion de strings
+
+# Definicion de variables
+num1 = 15
+num2 = 4
+
+# Operaciones Basicas
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(float(num1 / num2))
+print(num1 % num2)
+print(num1 // num2)
+print(num1 ** num2)
+
+# Variables para concatenacion de strings
+nombre = 'Gaston'
+apellido = 'Buedo'
+nombre_completo = nombre + ' ' + apellido
+print(f"El nombre completo concatenado es: {nombre_completo}")
+print(f"La longitud del completo concatenado es: {len(nombre_completo)}")
